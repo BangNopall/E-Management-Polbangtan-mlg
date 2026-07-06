b0598d4dd30f72fb5d82c233ba88ae8ad46a67b0
BangNopall <craft6254@gmail.com>
2026-07-06 14:10:19 +0700

    chore: update build assets and sync application configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 
+import * as Popper from '@popperjs/core';
+window.Popper = Popper;
+
 import 'flowbite';
 
 import './script';
