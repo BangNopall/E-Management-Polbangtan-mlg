6731d7eb5b071a87f0e63151016561e7bb8a6aaa
BangNopall <craft6254@gmail.com>
2026-08-04 21:04:33 +0700

    feat: add Pembina role to system flow documentation and admin user management

--- resources/views/admin/admin-edit/create-data-petugas.blade.php
+++ resources/views/admin/admin-edit/create-data-petugas.blade.php
@@ -76,6 +76,7 @@
                           <option value="1">Admin</option>
                           <option value="2">Operator</option>
                           <option value="4">Pelatih</option>
+                          <option value="5">Pembina</option>
                         </select>
                     </div>
                     <div class="relative z-0 w-full mb-6 group">
