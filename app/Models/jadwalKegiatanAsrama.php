811ceab425a794523f75fa91ea57ab36340dd05b
BangNopall <craft6254@gmail.com>
2026-08-18 04:22:33 +0700

    refactor: rename KegiatanAsramaController and JadwalKegiatanAsrama classes to follow PSR-1 naming conventions

--- app/Models/jadwalKegiatanAsrama.php
+++ app/Models/jadwalKegiatanAsrama.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class jadwalKegiatanAsrama extends Model
+class JadwalKegiatanAsrama extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
